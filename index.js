c0111cb4ac2c138e252d29915d91e6cc5989fd8c
gatinhap <72195350+gatinhap@users.noreply.github.com>
2022-12-29 19:41:04 +0100

    all passenger counter files added

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0
+let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let savedCount = count + " - ";
+    saveEl.textContent += savedCount
+    count = 0
+    countEl.textContent = count
+}
+
